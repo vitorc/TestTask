5d28653f06ddb27c3a6bace09c98b78fb9acdd1d
Vitor Cardoso dos Santos <VSantos@getcaret.com>
2023-12-11 16:35:06 -0300

    Create automation test for UI and API

---
+++ StudyGroups/Test/API/StudyGroupAPI.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Microsoft.Playwright.NUnit;
+using Microsoft.Playwright;
+using NUnit.Framework;
+using System.Net;
+
+namespace StudyGroups.Test
+{
+    public class StudyGroupApiTest
+    {
+
+        [Test]
+        public async Task CreateStudyGroup_ValidData_Succeeds()
+        {
+            var playwright = await Playwright.CreateAsync();
+            var requestContext = await playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions
+            {
+                BaseURL = "http://localhost:5000/api",
+            });
+
+            const string endpoint = "/api/study-groups";
+
+
+            var requestBody = new
+            {
+                name = "Awesome Math Group",
+                subject = "Math"
+            };
+
+
+            var response = await requestContext.PostAsync(endpoint, new APIRequestContextOptions()
+            {
+                DataObject = requestBody
+            });
+
+            Assert.Pass();
+
+        }
+        [Test]
+        public async Task CreateStudyGroup_InvalidName_Fails()
+        {
+
+            var playwright = await Playwright.CreateAsync();
+            var requestContext = await playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions
+            {
+                BaseURL = "http://localhost:5000/api",
+            });
+            const string endpoint = "/api/study-groups";
+            var requestBody = new
+            {
+                name = "Invalid",
+                subject = "Physics"
+            };
+
+
+            var response = await requestContext.PostAsync(endpoint, new APIRequestContextOptions()
+            {
+                DataObject = requestBody
+            });
+            Assert.Pass();
+
+
+        }
+
+        [Test]
+        public async Task CreateStudyGroup_DuplicateSubject_Fails()
+        {
+
+            var playwright = await Playwright.CreateAsync();
+            var requestContext = await playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions
+            {
+                BaseURL = "http://localhost:5000/api",
+            });
+            const string endpoint = "/api/study-groups";
+
+            var requestBody = new
+            {
+                name = "Another Math Group",
+                subject = "Math"
+            };
+
+
+            var response = await requestContext.PostAsync(endpoint, new APIRequestContextOptions()
+            {
+                DataObject = requestBody
+            });
+
+            
+        }
+    }
+}
---
+++ StudyGroups/Test/UI/StudyGroup.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Microsoft.Playwright.NUnit;
+using Microsoft.Playwright;
+using NUnit.Framework;
+
+
+namespace StudyGroups.Test.UI
+{
+    public class StudyGroup
+    {
+        private IPage _page;
+
+        [OneTimeSetUp]
+        public
+
+async Task Setup()
+        {
+            var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync();
+            _page = await browser.NewPageAsync();
+        }
+
+        [OneTimeTearDown]
+        public async Task Teardown()
+        {
+            await _page.CloseAsync();
+        }
+
+        [Test]
+        public async Task CreateStudyGroup_ValidData_Succeeds()
+        {
+
+            var name = "Awesome Math Group";
+            var subject = "Math";
+
+
+            await _page.GotoAsync("http://localhost:5000/study-groups/create");
+            await _page.FillAsync("#name", name);
+            await _page.SelectOptionAsync("#subject", subject.ToString());
+            await _page.ClickAsync("button[type='submit']");
+
+
+            var successMessage = await _page.TextContentAsync("#success-message");
+            Assert.That(successMessage, Is.EqualTo($"Study group '{name}' created successfully!"));
+        }
+
+        [Test]
+        public async Task CreateStudyGroup_InvalidName_Fails()
+        {
+
+            var name = "Invalid";
+            var subject = "Physics";
+
+
+            await _page.GotoAsync("http://localhost:5000/study-groups/create");
+            await _page.FillAsync("#name", name);
+            await _page.SelectOptionAsync("#subject", subject.ToString());
+            await _page.ClickAsync("button[type='submit']");
+
+
+            var errorMessage = await _page.TextContentAsync("#error-message");
+            Assert.That(errorMessage, Contains.Substring("Name must be between 5 and 30 characters"));
+        }
+
+        [Test]
+        public async Task CreateStudyGroup_DuplicateSubject_Fails()
+        {
+
+            var name = "Another Math Group";
+            var subject = "Math";
+
+
+            await _page.GotoAsync("http://localhost:5000/study-groups/create");
+            await _page.FillAsync("#name", name);
+            await _page.SelectOptionAsync("#subject", subject.ToString());
+            await _page.ClickAsync("button[type='submit']");
+
+
+            var errorMessage = await _page.TextContentAsync("#error-message");
+            Assert.That(errorMessage, Contains.Substring("You already have a study group for this subject"));
+        }
+    }
+}
